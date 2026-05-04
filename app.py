3fa61edea92db191c7f438a539f474c663d9eb67
Marcelo Massashi Simonae <msimonae@gmail.com>
2026-05-03 23:06:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="UTFPResponde V13", page_icon="🎓", layout="cent
 
 # URL da sua API hospedada no Render (Certifique-se de que é a URL correta do backend)
 #API_URL = "https://utfpresponde.onrender.com/chat"
-API_URL = "https://utfpresponde-backend-814505118980.europe-west1.run.app/chat"
+API_URL = "https://utfpresponde-814505118980.us-central1.run.app/chat"
 
 # Cabeçalhos alinhados com a narrativa da dissertação
 st.title("🎓 UTFPResponde (V13)")
